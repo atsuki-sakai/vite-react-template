0aaff7164d7ca3ee3d943473bf3f5784f0f2b323
Atsuki Sakai <atk721@icloud.com>
2025-08-04 15:54:57 +0900

    feat: App.tsxを更新 - メインアプリケーションコンポーネントを拡張

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import DatasetDetail from "./components/DatasetDetail";
 import DocumentDetail from "./components/DocumentDetail";
+import ChatHistory from "./components/ChatHistory";
 
 import HomePage from "./components/HomePage";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/chat-history" element={<ChatHistory />} />
       <Route path="/datasets/:datasetId" element={<DatasetDetail />} />
       <Route path="/datasets/:datasetId/documents/:documentId" element={<DocumentDetail />} />
     </Routes>
